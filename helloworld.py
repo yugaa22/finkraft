3adaf309aa9717d6ff3cd019b5fcc196f671bbe6
giribabuthanikonda <52537095+girichinna27@users.noreply.github.com>
2023-10-21 11:24:35 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    print(i)  # Corrected indentation
     return 'Hello World'
 
-if __name__ == "__main__":
+
+if _name_ == '__main__':
+
     app.run(debug=True, host='0.0.0.0')
