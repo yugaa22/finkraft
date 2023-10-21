378896bb59f4090fa003a734b8b70526bbaee8c4
Yugandharkumar <yugaa22@gmail.com>
2023-10-21 11:47:32 +0530

    Create test_helloworld.py

---
+++ test_helloworld.py
@@ -0,0 +1,16 @@
+import unittest
+from app import helloworld
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    def test_index(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'Hello World')
+
+if __name__ == '__main__':
+    unittest.main()
